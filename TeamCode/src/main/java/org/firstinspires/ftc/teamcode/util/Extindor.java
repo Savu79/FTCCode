49ed107d2324a8dba2357f1039e31e2a0b28c9a8
Parintele_Andu <andugolovatai@gmail.com>
2023-01-29 14:00:38 +0200

    group test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Extindor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Extindor.java
@@ -1,20 +1,32 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+@Config
+
 @TeleOp(name="Extindor", group="drive")
+
 public class Extindor extends LinearOpMode {
+
     DcMotorEx ExtindorSt;
     DcMotorEx ExtindorDr;
-    int EInchis=0;
-    int EDeschis=3400;
+    public static int EInchis=0;
+    public static int EDeschis=3400;
     int pozESt=EInchis;
     int pozEDr=EInchis;
+    public static double Kp=1;
+    @Override
     public void runOpMode(){
+
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
 
         ExtindorDr= hardwareMap.get(DcMotorEx.class, "ExtindorDr");
         ExtindorSt= hardwareMap.get(DcMotorEx.class, "ExtindorSt");
@@ -23,18 +35,11 @@ public class Extindor extends LinearOpMode {
 
         waitForStart();
 
-
-        ExtindorSt.setTargetPosition(EInchis);
-        ExtindorDr.setTargetPosition(EInchis);
-        ExtindorSt.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        ExtindorDr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        ExtindorDr.setPower(0.4);
-        ExtindorSt.setPower(0.4);
-
         while(opModeIsActive())
         {
-            ExtindorSt.setTargetPosition(pozESt);
-            ExtindorDr.setTargetPosition(pozEDr);
+            ExtindorSt.setPower((pozESt-ExtindorSt.getCurrentPosition())*Kp);
+            ExtindorDr.setPower((pozEDr-ExtindorDr.getCurrentPosition())*Kp);
+
             if(gamepad1.x) {
                 pozESt=EInchis;
                 pozEDr=EInchis;
@@ -48,6 +53,7 @@ public class Extindor extends LinearOpMode {
                 ExtindorSt.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 ExtindorDr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             }
+
             if(gamepad1.right_stick_y!=0)
             {
                 pozESt+=-gamepad1.right_stick_y*20;
