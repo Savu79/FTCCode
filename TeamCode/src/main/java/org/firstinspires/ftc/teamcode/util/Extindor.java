62f2ef410a5f6c1f81dde96872f139ebc7c5d197
Parintele_Andu <andugolovatai@gmail.com>
2023-01-30 13:50:02 +0200

    group test 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Extindor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Extindor.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 @Config
@@ -16,15 +17,28 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Extindor extends LinearOpMode {
 
+
     DcMotorEx ExtindorSt;
     DcMotorEx ExtindorDr;
     public static int EInchis=0;
     public static int EDeschis=3550;
+    int errorSt;
+    int errorDr;
+    double integralSumDr;
+    double integralSumSt;
+    double derivativeSt;
+    double derivativeDr;
+    int lastErrorSt=0;
+    int lastErrorDr=0;
     int pozESt=EInchis;
     int pozEDr=EInchis;
     public static double Kp=0.01;
+    public static double Ki=0;
+    public static double Kd=0;
     @Override
     public void runOpMode(){
+
+
 
         Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
 
@@ -34,12 +48,19 @@ public class Extindor extends LinearOpMode {
         ExtindorSt.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
-
+        ElapsedTime timer = new ElapsedTime();
         while(opModeIsActive())
         {
-            ExtindorSt.setPower((pozESt-ExtindorSt.getCurrentPosition())*Kp);
-            ExtindorDr.setPower((pozEDr-ExtindorDr.getCurrentPosition())*Kp);
-
+            errorSt=pozESt-ExtindorSt.getCurrentPosition();
+            errorDr=pozEDr-ExtindorDr.getCurrentPosition();
+            integralSumSt = integralSumSt + (errorSt * timer.seconds());
+            integralSumDr = integralSumDr + (errorDr * timer.seconds());
+            derivativeSt = (errorSt-lastErrorSt) / timer.seconds();
+            derivativeDr = (errorDr-lastErrorDr) / timer.seconds();
+            ExtindorSt.setPower(  (errorSt*Kp)  +  (integralSumSt*Ki)  + (derivativeSt*Kd)  );
+            ExtindorDr.setPower(  (errorDr*Kp)  +  (integralSumDr*Ki)  + (derivativeDr*Kd)  );
+            lastErrorSt=errorSt;
+            lastErrorDr=errorDr;
             if(gamepad1.x) {
                 pozESt=EInchis;
                 pozEDr=EInchis;
